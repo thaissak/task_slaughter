631ec20c4fdd5a0346e8156d1c02af44dc038a1c
thaissak <thaissa.kobayashi@gmail.com>
2016-07-01 09:09:25 -0700

    minor fix to future tasks query

--- app/models/Schedule.py
+++ app/models/Schedule.py
@@ -68,7 +68,7 @@ class Schedule(Model):
         return self.db.query_db(query, data)   
 
     def get_future_tasks(self, user_id):
-        query = "SELECT tasks.id, tasks.status, tasks.task_name, tasks.description, date_format(tasks.date, '%b %d, %Y') as date, date_format(tasks.time,'%k:%i') as time, locations.location_name, locations.street_name, locations.city, locations.state, locations.zip_code FROM tasks JOIN locations ON tasks.location_id = locations.id JOIN users on tasks.user_id = users.id WHERE users.id=:id and tasks.date >= CURDATE() ORDER BY tasks.date DESC, tasks.time DESC"
+        query = "SELECT tasks.id, tasks.status, tasks.task_name, tasks.description, date_format(tasks.date, '%b %d, %Y') as date, date_format(tasks.time,'%k:%i') as time, locations.location_name, locations.street_name, locations.city, locations.state, locations.zip_code FROM tasks JOIN locations ON tasks.location_id = locations.id JOIN users on tasks.user_id = users.id WHERE users.id=:id and tasks.date > CURDATE() ORDER BY tasks.date ASC, tasks.time ASC"
         data = {'id' : user_id}
         return self.db.query_db(query, data) 
 
